cedd9f26df53d8cb58d5b0bf39004cacfb8e8bb3
Sohaib Ahsan <125086696+Sohaib-Ahsan22@users.noreply.github.com>
2025-03-12 18:37:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,6 @@ def download_file(filename):
     return send_file(os.path.join(app.config['OUTPUT_FOLDER'], filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Railway will assign a dynamic port
+    app.run(host="0.0.0.0", port=port)
